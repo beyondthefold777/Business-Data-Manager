0f564d5875bc14d2023d11b81149e5a284d4aecd
beyondthefold777 <beyondthefold1989@gmail.com>
2024-06-19 14:08:32 -0400

    adding departments in the seeds sql file for a software engineering company

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (department_name)
+VALUES ("Marketing"),
+       ("HR Department"),
+       ("Business Development"),
+       ("IT Department"),
+       ("UX/UI Department"),
+       ("Accounting"),
