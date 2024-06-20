d4908941af8a66ba43fa29ed7e6811a8221c668f
beyondthefold777 <beyondthefold1989@gmail.com>
2024-06-20 09:08:34 -0400

    added connection string to env for security, removed node modules from repo and added to gitignore, installed dotenv

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,21 @@ VALUES ("Marketing"),
        ("IT Department"),
        ("UX/UI Department"),
        ("Accounting"),
+       ("Board of Directors");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Full Stack Developer", 115000, IT)
+    ("Project Manager", 120000, IT)
+    ("UX/UI Specialist", 100000, UI/UX)
+    ("Accountant", 100000, ACCT)
+    ("CEO", 200000, BD)
+    ("Software Marketing Manager", 100000, MKT)
+
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("anthony", "bell", Full Stack Developer), 
+       ("edwin", "rivera", UX/UX Specialist),
+        ("lindsey", "clapp", Project Manager),
+         ("maria", "roa", Software marketing manager),
+          ("juan", "parra", Accountant),
+          ("marcio", "pimentel", CEO);
