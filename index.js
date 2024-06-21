360551775b97c9ac6b963d847763a6352727f818
beyondthefold777 <beyondthefold1989@gmail.com>
2024-06-20 21:17:40 -0400

    fixing errors & commenting for clarity

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
-require('dotenv').config();
+require('dotenv').config(); //loading environment variables
 const CFonts = require('cfonts');
 const inquirer = require("inquirer");
-
+// Creating a new database connection pool using the provided connection string
 const pool = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
 });
-
+// Connecting to the database and starting the application
+pool.connect()
+  .then(() => {
+    console.log('Connected to the database');
+    startApp();
+  })
+  .catch((err) => console.error('Error connecting to database', err));
+//checking connection status by querying the database
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Error connecting to database', err.stack);
@@ -72,29 +79,123 @@ const startApp = async () => {
 
   switch (response.action) {
     case 'View all departments':
-      // Implement view all departments logic
+      viewAllDepartments();
       break;
     case 'View all roles':
-      // Implement view all roles logic
+      viewAllRoles();
       break;
     case 'View all employees':
-      // Implement view all employees logic
+      viewAllEmployees();
       break;
     case 'Add a department':
-      // Implement add a department logic
+      addDepartment();
       break;
     case 'Add a role':
-      // Implement add a role logic
+      addRole();
       break;
     case 'Add an employee':
-      // Implement add an employee logic
+      addEmployee();
       break;
     case 'Update an employee role':
-      // Implement update an employee role logic
+      updateEmployeeRole();
       break;
     default:
       console.log('Invalid action');
   }
 };
+
+async function viewAllDepartments() {
+  const query = 'SELECT * FROM departments';
+  const { rows } = await pool.query(query);
+  console.log('All Departments:');
+  console.table(rows);
+}
+
+async function viewAllRoles() {
+  const query = 'SELECT title, salary, department_id FROM roles';
+  const { rows } = await pool.query(query);
+  console.log('All Roles:');
+  console.table(rows);
+}
+
+async function viewAllEmployees() {
+  const query = 'SELECT first_name, last_name, role_id, manager_id FROM employees';
+  const { rows } = await pool.query(query);
+  console.log('All Employees:');
+  console.table(rows);
+}
+
+async function addDepartment() {
+  const newDepartment = await inquirer.prompt({
+    type: 'input',
+    name: 'department_name',
+    message: 'Enter the department name:'
+  });
+
+  const query = 'INSERT INTO departments (department_name) VALUES ($1) RETURNING *';
+  const { rows } = await pool.query(query, [newDepartment.department_name]);
+  console.log('New Department added:');
+  console.table(rows);
+}
+
+async function addRole() {
+  const newRole = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the role title:'
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'Enter the role salary:'
+    },
+    {
+      type: 'input',
+      name: 'department_id',
+      message: 'Enter the department ID:'
+    }
+  ]);
+
+  const query = 'INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *';
+  const { rows } = await pool.query(query, [newRole.title, newRole.salary, newRole.department_id]);
+  console.log('New Role added:');
+  console.table(rows);
+}
+
+async function addEmployee() {
+  const newEmployee = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'Enter the employee first name:'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Enter the employee last name:'
+    },
+    {
+      type: 'input',
+      name: 'role_id',
+      message: 'Enter the role ID:'
+    },
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: 'Enter the manager ID (if applicable):'
+    }
+  ]);
+
+  const query = 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *';
+  const { rows } = await pool.query(query, [newEmployee.first_name, newEmployee.last_name, newEmployee.role_id, newEmployee.manager_id]);
+  console.log('New Employee added:');
+  console.table(rows);
+}
+
+async function updateEmployeeRole() {
+  // Implement update employee role logic
+  console.log('Update employee role logic to be implemented');
+}
 
 startApp();
